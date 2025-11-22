c9d8c7bed4e209026c15226536fddfb2aad60869
Danie <danielhance02@gmail.com>
2025-11-22 20:08:25 +0000

    Buttons part 1

--- Objects/Player/player.gd
+++ Objects/Player/player.gd
@@ -70,7 +70,8 @@ func _physics_process(delta: float) -> void:
 		)
 		
 	if Input.is_action_just_pressed("unlock (debug)"):
-		DialogueManager.unlock()
+		#DialogueManager.unlock()
+		DialogueManager._start_dialogue_buttons(["Option 1", "Option 2", "Option 3"], [1, 2, 3])
 		
 		
 		
