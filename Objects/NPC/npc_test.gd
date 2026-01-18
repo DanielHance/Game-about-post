fa8c34e12407dd1f76d2b0df8e894dc0d1d1a231
Danie <danielhance02@gmail.com>
2026-01-18 16:45:23 +0000

    NPC Fix

--- Objects/NPC/npc_test.gd
+++ Objects/NPC/npc_test.gd
@@ -10,22 +10,25 @@ func _ready() -> void:
 	connect("body_entered", Callable(self, "_on_area_enter"))
 	connect("body_exited", Callable(self, "_on_area_exit"))
 	
-	if sprite and sprite.sprite_frames:
-		if sprite.sprite_frames.has_animation("idel"):
-			sprite.play("idel")
+	if sprite:
+		sprite.play()
+		
+	#if sprite and sprite.sprite_frames:
+		#if sprite.sprite_frames.has_animation("idel"):
+			#sprite.play("idel")
 		
 func _on_area_enter(body):
 	if body.name == "Player":
 		player_in_range = true
 		body.current_npc = self
-		if sprite:
-			if sprite.sprite_frames.has_animation("wave"):  #Or whatever animation to play when able to talk to player
-				sprite.play("wave")
+		#if sprite:
+			#if sprite.sprite_frames.has_animation("wave"):  #Or whatever animation to play when able to talk to player
+				#sprite.play("wave")
 
 func _on_area_exit(body):
 	if body.name == "Player":
 		player_in_range = false
 		body.current_npc = null
-		if sprite:
-			if sprite.sprite_frames.has_animation("idel"):
-				sprite.play("idel")
+		#if sprite:
+			#if sprite.sprite_frames.has_animation("idel"):
+				#sprite.play("idel")
